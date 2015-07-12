ab9058489261085fbe295d0483a3f7fd3daedde2
Joseph Roque <github@josephroque.ca>
2015-07-12 13:37:43 -0400

    Drawing frames around text

--- src/game_editor.c
+++ src/game_editor.c
@@ -3,7 +3,6 @@
 #include "utils.h"
   
 static uint8_t s_current_pin = 0;
-static bool s_pin_knocked[5] = {false, false, false, false, false};
 static PropertyAnimation *s_property_animation;
   
 static GBitmap *s_indicator_bitmap;
@@ -20,7 +19,10 @@ static char* bowler_name;
 static char* league_name;
 static char* series_name;
 static char* game_title;
-static uint8_t currentGame = 0;
+static uint8_t s_current_game = 0;
+static uint8_t s_current_frame = 0;
+static uint8_t s_current_ball = 0;
+static bool s_pin_state[150];
 
 // BEGIN AUTO-GENERATED UI CODE; DO NOT MODIFY
 static Window *s_window;
@@ -49,11 +51,56 @@ static BitmapLayer *s_bitmap_pin_4;
 static BitmapLayer *s_bitmap_indicator;
 static BitmapLayer *s_bitmap_background;
 
+static GPath *s_text_background_path_1 = NULL;
+static GPath *s_text_background_path_2 = NULL;
+static GPath *s_text_background_path_3 = NULL;
+static GPath *s_text_background_path_4 = NULL;
+
+static const GPathInfo BOLT_PATH_INFO_1 = {
+  .num_points = 26,
+  .points = (GPoint []) {{0, 63},{16, 63},{16, 45},{16, 63},{32, 63},{32, 45},{32, 63},{47, 63},{47, 45},{47, 63},{64, 63},
+                        {64, 45},{64, 63},{80, 63},{80, 45},{80, 63},{96, 63},{96, 45},{96, 63},{112, 63},{112, 45},{112, 63},
+                        {128, 63},{128, 45},{128, 63},{143, 63}}
+};
+
+static const GPathInfo BOLT_PATH_INFO_2 = {
+  .num_points = 2,
+  .points = (GPoint []) {{47, 45},{47,78}}
+};
+
+static const GPathInfo BOLT_PATH_INFO_3 = {
+  .num_points = 2,
+  .points = (GPoint []) {{96, 45},{96,78}}
+};
+
+static const GPathInfo BOLT_PATH_INFO_4 = {
+  .num_points = 5,
+  .points = (GPoint []) {{0, 45},{0,78},{143,78},{143,45},{0,45}}
+};
+
+void setup_paths(void) {
+  s_text_background_path_1 = gpath_create(&BOLT_PATH_INFO_1);
+  s_text_background_path_2 = gpath_create(&BOLT_PATH_INFO_2);
+  s_text_background_path_3 = gpath_create(&BOLT_PATH_INFO_3);
+  s_text_background_path_4 = gpath_create(&BOLT_PATH_INFO_4);
+}
+
+static void text_bg_draw(Layer *my_layer, GContext* ctx) {
+  // Stroke the path:
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  gpath_draw_outline(ctx, s_text_background_path_1);
+  gpath_draw_outline(ctx, s_text_background_path_2);
+  gpath_draw_outline(ctx, s_text_background_path_3);
+  gpath_draw_outline(ctx, s_text_background_path_4);
+}
+
 static void initialise_ui(void) {
   s_window = window_create();
   #ifndef PBL_SDK_3
     window_set_fullscreen(s_window, true);
   #endif
+    
+  setup_paths();
     
   s_indicator_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_INDICATOR);
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
@@ -85,86 +132,87 @@ static void initialise_ui(void) {
   text_layer_set_font(s_textlayer_league, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_league);
   
-  // s_layer_frame_bg
-  s_layer_frame_bg = layer_create(GRect(0, 45, 144, 36));
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_layer_frame_bg);
-  
   // s_textlayer_f1_score
   s_textlayer_f1_score = text_layer_create(GRect(0, 63, 47, 15));
-  text_layer_set_text(s_textlayer_f1_score, "999");
+  text_layer_set_text(s_textlayer_f1_score, "0");
   text_layer_set_text_alignment(s_textlayer_f1_score, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f1_score, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f1_score);
   
   // s_textlayer_f2_score
   s_textlayer_f2_score = text_layer_create(GRect(48, 63, 48, 15));
-  text_layer_set_text(s_textlayer_f2_score, "999");
+  text_layer_set_text(s_textlayer_f2_score, "0");
   text_layer_set_text_alignment(s_textlayer_f2_score, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f2_score, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f2_score);
   
   // s_textlayer_f3_score
   s_textlayer_f3_score = text_layer_create(GRect(97, 63, 47, 15));
-  text_layer_set_text(s_textlayer_f3_score, "999");
+  text_layer_set_text(s_textlayer_f3_score, "0");
   text_layer_set_text_alignment(s_textlayer_f3_score, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f3_score, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f3_score);
   
   // s_textlayer_f1_b1
   s_textlayer_f1_b1 = text_layer_create(GRect(0, 45, 15, 15));
-  text_layer_set_text(s_textlayer_f1_b1, "Hp");
+  text_layer_set_text(s_textlayer_f1_b1, "-");
   text_layer_set_text_alignment(s_textlayer_f1_b1, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f1_b1, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f1_b1);
   
   // s_textlayer_f1_b2
   s_textlayer_f1_b2 = text_layer_create(GRect(16, 45, 15, 15));
-  text_layer_set_text(s_textlayer_f1_b2, "Hp");
+  text_layer_set_text(s_textlayer_f1_b2, "-");
   text_layer_set_text_alignment(s_textlayer_f1_b2, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f1_b2, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f1_b2);
   
   // s_textlayer_f1_b3
   s_textlayer_f1_b3 = text_layer_create(GRect(32, 45, 15, 15));
-  text_layer_set_text(s_textlayer_f1_b3, "Hp");
+  text_layer_set_text(s_textlayer_f1_b3, "-");
   text_layer_set_text_alignment(s_textlayer_f1_b3, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f1_b3, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f1_b3);
   
   // s_textlayer_f2_b1
   s_textlayer_f2_b1 = text_layer_create(GRect(48, 45, 15, 15));
-  text_layer_set_text(s_textlayer_f2_b1, "Hp");
+  text_layer_set_text(s_textlayer_f2_b1, "-");
   text_layer_set_text_alignment(s_textlayer_f2_b1, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f2_b1, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f2_b1);
   
   // s_textlayer_f2_b2
   s_textlayer_f2_b2 = text_layer_create(GRect(64, 45, 15, 15));
-  text_layer_set_text(s_textlayer_f2_b2, "Hp");
+  text_layer_set_text(s_textlayer_f2_b2, "-");
+  text_layer_set_text_alignment(s_textlayer_f2_b2, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f2_b2, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f2_b2);
   
   // s_textlayer_f2_b3
   s_textlayer_f2_b3 = text_layer_create(GRect(80, 45, 15, 15));
-  text_layer_set_text(s_textlayer_f2_b3, "Hp");
+  text_layer_set_text(s_textlayer_f2_b3, "-");
+  text_layer_set_text_alignment(s_textlayer_f2_b3, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f2_b3, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f2_b3);
   
   // s_textlayer_f3_b1
   s_textlayer_f3_b1 = text_layer_create(GRect(96, 45, 15, 15));
-  text_layer_set_text(s_textlayer_f3_b1, "Hp");
+  text_layer_set_text(s_textlayer_f3_b1, "-");
+  text_layer_set_text_alignment(s_textlayer_f3_b1, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f3_b1, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f3_b1);
   
   // s_textlayer_f3_b2
   s_textlayer_f3_b2 = text_layer_create(GRect(112, 45, 15, 15));
-  text_layer_set_text(s_textlayer_f3_b2, "Hp");
+  text_layer_set_text(s_textlayer_f3_b2, "-");
+  text_layer_set_text_alignment(s_textlayer_f3_b2, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f3_b2, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f3_b2);
   
   // s_textlayer_f3_b3
   s_textlayer_f3_b3 = text_layer_create(GRect(128, 45, 15, 15));
-  text_layer_set_text(s_textlayer_f3_b3, "Hp");
+  text_layer_set_text(s_textlayer_f3_b3, "-");
+  text_layer_set_text_alignment(s_textlayer_f3_b3, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_f3_b3, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_f3_b3);
   
@@ -192,6 +240,11 @@ static void initialise_ui(void) {
   s_bitmap_indicator = bitmap_layer_create(GRect(0, 153, 28, 15));
   bitmap_layer_set_bitmap(s_bitmap_indicator, s_indicator_bitmap);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_bitmap_indicator);
+  
+  // s_layer_frame_bg
+  s_layer_frame_bg = layer_create(GRect(0, 0, 144, 168));
+  layer_set_update_proc(s_layer_frame_bg, text_bg_draw);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_layer_frame_bg);
   
   #ifdef PBL_PLATFORM_APLITE
     bitmap_layer_set_bitmap(s_bitmap_pin_0, s_pin_enabled_bitmap_white);
@@ -259,6 +312,24 @@ static void destroy_ui(void) {
 }
 // END AUTO-GENERATED UI CODE
 
+static bool is_pin_knocked(uint8_t frame, uint8_t ball, uint8_t pin) {
+  return s_pin_state[frame * 15 + ball * 5 + pin];
+}
+
+static void set_pin_knocked(uint8_t frame, uint8_t ball, uint8_t pin, bool knocked) {
+  s_pin_state[frame * 15 + ball * 5 + pin] = knocked;
+}
+
+static void update_all_textboxes() {
+  //s_textlayer_f1_b1 - f3_b3
+  //s_textlayer_f1_score - s_textlayer_f3_score
+}
+
+static void update_last_textboxes() {
+  //s_textlater_f3_b1 - s_textlater_f3_b3
+  //s_textlayer_f3_score
+}
+
 static void on_animation_stopped(Animation *anim, bool finished, void *context)
 {
   //Free the memory used by the Animation
@@ -266,10 +337,10 @@ static void on_animation_stopped(Animation *anim, bool finished, void *context)
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Animation destroyed");
 }
 
-static void set_game(uint8_t newGame) {
-  currentGame = newGame + 1;
+static void set_game(uint8_t new_game) {
+  s_current_game = new_game + 1;
   const char* game_boiler = "Game #";
-  const char* game_num = itoa(currentGame);
+  const char* game_num = itoa(s_current_game);
 
   if (game_title) {
     free(game_title);
@@ -279,14 +350,11 @@ static void set_game(uint8_t newGame) {
   strcpy(game_title, game_boiler);
   strcat(game_title, game_num);
   text_layer_set_text(s_textlayer_gamenum, game_title);
-  currentGame--;
+  s_current_game--;
 }
 
-static void update_pin_status(BitmapLayer *pin_bitmap, int8_t pin_to_update, bool knocked) {
-  if (s_pin_knocked[pin_to_update] == knocked)
-    return;
-  
-  s_pin_knocked[pin_to_update] = knocked;
+static void update_pin_status(BitmapLayer *pin_bitmap, uint8_t frame, uint8_t ball, uint8_t pin, bool knocked) {
+  set_pin_knocked(frame, ball, pin, knocked);
   if (knocked) {
     #ifdef PBL_PLATFORM_APLITE
       bitmap_layer_set_bitmap(pin_bitmap, s_pin_disabled_bitmap_white);
@@ -349,7 +417,24 @@ static void up_long_click_handler(ClickRecognizerRef recognizer, void *context) 
 }
 
 static void up_long_click_release_handler(ClickRecognizerRef recognizer, void *context) {
-  // TODO: prev frame
+  if (s_current_ball == 0) {
+    if (s_current_frame == 0)
+      return;
+    else {
+      s_current_frame--;
+      s_current_ball = 2;
+    }
+  } else {
+    s_current_ball--;
+  }
+  
+  s_current_pin = 0;
+  update_indicator_position();
+  update_pin_status(s_bitmap_pin_0, s_current_frame, s_current_ball, 0, is_pin_knocked(s_current_frame, s_current_ball, 0));
+  update_pin_status(s_bitmap_pin_1, s_current_frame, s_current_ball, 1, is_pin_knocked(s_current_frame, s_current_ball, 1));
+  update_pin_status(s_bitmap_pin_2, s_current_frame, s_current_ball, 2, is_pin_knocked(s_current_frame, s_current_ball, 2));
+  update_pin_status(s_bitmap_pin_3, s_current_frame, s_current_ball, 3, is_pin_knocked(s_current_frame, s_current_ball, 3));
+  update_pin_status(s_bitmap_pin_4, s_current_frame, s_current_ball, 4, is_pin_knocked(s_current_frame, s_current_ball, 4));
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -364,7 +449,24 @@ static void down_long_click_handler(ClickRecognizerRef recognizer, void *context
 }
 
 static void down_long_click_release_handler(ClickRecognizerRef recognizer, void *context) {
-  // TODO: next frame
+  if (s_current_ball == 2) {
+    if (s_current_frame == 9)
+      return;
+    else {
+      s_current_frame++;
+      s_current_ball = 0;
+    }
+  } else {
+    s_current_ball++;
+  }
+  
+  s_current_pin = 0;
+  update_indicator_position();
+  update_pin_status(s_bitmap_pin_0, s_current_frame, s_current_ball, 0, is_pin_knocked(s_current_frame, s_current_ball, 0));
+  update_pin_status(s_bitmap_pin_1, s_current_frame, s_current_ball, 1, is_pin_knocked(s_current_frame, s_current_ball, 1));
+  update_pin_status(s_bitmap_pin_2, s_current_frame, s_current_ball, 2, is_pin_knocked(s_current_frame, s_current_ball, 2));
+  update_pin_status(s_bitmap_pin_3, s_current_frame, s_current_ball, 3, is_pin_knocked(s_current_frame, s_current_ball, 3));
+  update_pin_status(s_bitmap_pin_4, s_current_frame, s_current_ball, 4, is_pin_knocked(s_current_frame, s_current_ball, 4));
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -388,7 +490,7 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
       break;
   }
 
-  update_pin_status(active_pin, s_current_pin, !s_pin_knocked[s_current_pin]);
+  update_pin_status(active_pin, s_current_frame, s_current_ball, s_current_pin, !is_pin_knocked(s_current_frame, s_current_ball, s_current_pin));
 }
 
 static void select_long_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -396,11 +498,11 @@ static void select_long_click_handler(ClickRecognizerRef recognizer, void *conte
 }
 
 static void select_long_click_release_handler(ClickRecognizerRef recognizer, void *context) {
-  update_pin_status(s_bitmap_pin_0, 0x0, true);
-  update_pin_status(s_bitmap_pin_1, 0x1, true);
-  update_pin_status(s_bitmap_pin_2, 0x2, true);
-  update_pin_status(s_bitmap_pin_3, 0x3, true);
-  update_pin_status(s_bitmap_pin_4, 0x4, true);
+  update_pin_status(s_bitmap_pin_0, s_current_frame, s_current_ball, 0x0, true);
+  update_pin_status(s_bitmap_pin_1, s_current_frame, s_current_ball, 0x1, true);
+  update_pin_status(s_bitmap_pin_2, s_current_frame, s_current_ball, 0x2, true);
+  update_pin_status(s_bitmap_pin_3, s_current_frame, s_current_ball, 0x3, true);
+  update_pin_status(s_bitmap_pin_4, s_current_frame, s_current_ball, 0x4, true);
   
   // TODO: go to next frame
 }
